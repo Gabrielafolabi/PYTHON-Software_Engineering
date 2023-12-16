4855c64e59d0140b0fcb92d10a2b90fc6fc5385d
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-12-16 09:59:38 +0100

    updated

--- Password_Generator.py
+++ Password_Generator.py
@@ -12,33 +12,47 @@ print('HELLO! I AM ROBOT GABE, I GENERATE PASSWORD... HOW CAN I BE OF HELP? \n')
 Response = input('Would you like to generate a password?  \n Enter Yes/No:  \n')
 
 if Response == 'Yes' or 'YES':
-    no_letter = int(input('Enter the number of letters you desire:  \n'))
-    print('Please enter a digit') 
-    no_numbers = int(input('Enter the number of digits you desire:  \n'))
-        
-    print('Please enter a digit')
-    no_symbols = int(input('Enter the number of symbols you desire:  \n'))
-        
-    print('Please enter a digit')
+    while True:
+        no_letter = (input('Enter the number of letters you desire:  \n'))
+        if no_letter.isdecimal():
+            break
+        print('Please enter a digit')
+
+    while True:
+        no_numbers = (input('Enter the number of digits you desire:  \n'))
+        if no_numbers.isdecimal():
+            break
+        print('Please enter a digit')
+
+    while True:
+            
+        no_symbols = (input('Enter the number of symbols you desire:  \n'))
+        if no_symbols.isdecimal():
+            break
+        print('Please enter a digit')
 else:
     print('Thanks for visiting our sites')
 
+New_no_letter =int(no_letter)
+New_no_numbers = int(no_numbers)
+New_no_symbols = int(no_symbols)
+
 Pass_Letter=[]
-for i in range(1, no_letter+1):
+for i in range(1, New_no_letter+1):
     char = random.choice(Letters)
     Pass_Letter += char
 
-for i in range(1, no_numbers+1):
+for i in range(1, New_no_numbers+1):
     char = random.choice(Numbers)
     Pass_Letter += char
 
-for i in range(1, no_symbols+1):
+for i in range(1, New_no_symbols+1):
     char = random.choice(Symbols)
     Pass_Letter += char
 
-print(Pass_Letter)
+#print(Pass_Letter)
 random.shuffle(Pass_Letter)
-print(Pass_Letter)
+#print(Pass_Letter)
 
 New_Pass = ""
 for char in Pass_Letter:
